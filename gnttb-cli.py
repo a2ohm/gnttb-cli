bc634a8e748a24ed7de4ff97d264f5dd68d99342
Antoine <antoine@2ohm.fr>
2019-07-20 15:49:28 +0200

    add the compare subcommand (gnttb v0.2 required)

--- gnttb-cli.py
+++ gnttb-cli.py
@@ -4,13 +4,14 @@
 import argparse
 import sys
 
-from sub.search import search
+from sub.compare import compare
 from sub.get import get
+from sub.search import search
 
 # Check the gnttb version
 import gnttb
 
-gnttb_neededVersion = '0.1'
+gnttb_neededVersion = '0.2'
 if gnttb.__version__ < gnttb_neededVersion:
     print("You are using version {} of gnttb, but at least version {} is needed.".format(gnttb.__version__, gnttb_neededVersion))
     print("Please update gnttb (pip3 install --user --upgrade gnttb-a2ohm).")
@@ -20,15 +21,21 @@ if gnttb.__version__ < gnttb_neededVersion:
 parser = argparse.ArgumentParser(description="CLI of the Greek New Testament toolbox.")
 subparsers = parser.add_subparsers()
 
-# Create the parser for the 'search' command
-parser_search = subparsers.add_parser('search', help='search in the New Testament')
-parser_search.add_argument('lemma', help='return the concorance of lemma in the New Testament')
-parser_search.set_defaults(func=search) 
+# Create the parser for the 'compare' command
+parser_compare = subparsers.add_parser('compare', help='compare two verses')
+parser_compare.add_argument('bcv1', help='bcv of the first verse')
+parser_compare.add_argument('bcv2', help='bcv of the second verse')
+parser_compare.set_defaults(func=compare) 
 
 # Create the parser for the 'get' command
 parser_get = subparsers.add_parser('get', help='return a verse from the New Testament')
 parser_get.add_argument('bcv', help='bcv of the verse')
 parser_get.set_defaults(func=get) 
+
+# Create the parser for the 'search' command
+parser_search = subparsers.add_parser('search', help='search in the New Testament')
+parser_search.add_argument('lemma', help='return the concorance of lemma in the New Testament')
+parser_search.set_defaults(func=search) 
 
 # Parse
 args = parser.parse_args()
---
+++ sub/compare.py
@@ -0,0 +1,123 @@
+#! /usr/bin/python3
+# -*- coding:utf-8 -*-
+
+"""
+Define the get sub-command.
+"""
+
+import gnttb.compare
+
+from tools import bcv
+from tools.colors import colors
+
+def compare(args):
+    """Compare two verses given their bcv.
+    """
+
+    fg_emph = colors.fg.PINK
+    bg_emph = colors.bg.PINK
+
+    verse1 = gnttb.get.get(args.bcv1)
+    verse2 = gnttb.get.get(args.bcv2)
+
+    averse1, averse2 = gnttb.compare.compare(verse1, verse2)
+
+    # Display the comparaison in columns
+    colWidth = 42
+    buffSize = 0
+    buff1 = ""
+    buff2 = ""
+
+    isUsingColor = 0    # 0: no, 1: fg, 2: bg
+
+    print(" {c_emph}{:^{width}}{c_end} | {c_emph}{:^{width}}{c_end}".format(bcv.bcv2str(args.bcv1), bcv.bcv2str(args.bcv2),
+        c_emph = colors.BOLD, c_end = colors.END, width = colWidth))
+
+    for w1, w2 in zip(averse1, averse2):
+        ww1 = "" if w1 is None else w1['text']
+        ww2 = "" if w2 is None else w2['text']
+
+        wSize = max(len(ww1), len(ww2))
+
+        if buffSize + wSize > 42:
+            # News words do not fit in width.
+            # So display current buffers and reset them
+            # to create a new line.
+
+            # Properly end text formating
+            if isUsingColor > 0:
+                buff1 += colors.END
+                buff2 += colors.END
+                isUsingColor = 0
+
+            # Display buffers
+            print(" {}{} | {}".format(buff1, ' '*(colWidth-buffSize), buff2))
+
+            # and reset them
+            buff1 = ""
+            buff2 = ""
+            buffSize = 0
+
+        # Format text
+        if w1 is not None and w2 is not None:
+            if w1['norm'] == w2['norm']:
+                # Same word: strong emphasis
+                if isUsingColor == 0:
+                    isUsingColor = 2
+                    ww1 = "{{space}}{}{:{width}}".format(bg_emph, ww1, width = wSize)
+                    ww2 = "{{space}}{}{:{width}}".format(bg_emph, ww2, width = wSize)
+
+                elif isUsingColor == 1:
+                    isUsingColor = 2
+                    ww1 = "{}{{space}}{}{:{width}}".format(colors.END, bg_emph, ww1, width = wSize)
+                    ww2 = "{}{{space}}{}{:{width}}".format(colors.END, bg_emph, ww2, width = wSize)
+
+                else:
+                    ww1 = "{{space}}{:{width}}".format(ww1, width = wSize)
+                    ww2 = "{{space}}{:{width}}".format(ww2, width = wSize)
+
+            else:
+                # Same lemma: light emphasis
+                if isUsingColor == 0:
+                    isUsingColor = 1
+                    ww1 = "{{space}}{}{:{width}}".format(fg_emph, ww1, width = wSize)
+                    ww2 = "{{space}}{}{:{width}}".format(fg_emph, ww2, width = wSize)
+
+                elif isUsingColor == 2:
+                    isUsingColor = 1
+                    ww1 = "{}{{space}}{}{:{width}}".format(colors.END, fg_emph, ww1, width = wSize)
+                    ww2 = "{}{{space}}{}{:{width}}".format(colors.END, fg_emph, ww2, width = wSize)
+
+                else:
+                    ww1 = "{{space}}{:{width}}".format(ww1, width = wSize)
+                    ww2 = "{{space}}{:{width}}".format(ww2, width = wSize)
+
+        else:
+            if isUsingColor > 0:
+                isUsingColor = 0
+                ww1 = "{}{{space}}{:{width}}".format(colors.END, ww1, width = wSize)
+                ww2 = "{}{{space}}{:{width}}".format(colors.END, ww2, width = wSize)
+            else:
+                ww1 = "{{space}}{:{width}}".format(ww1, width = wSize)
+                ww2 = "{{space}}{:{width}}".format(ww2, width = wSize)
+
+        # Append words to their buffer
+        # adding a space if necessary
+        if buffSize == 0:
+            buff1 += ww1.format(space = '')
+            buff2 += ww2.format(space = '')
+            buffSize += wSize
+        else:
+            buff1 += ww1.format(space = ' ')
+            buff2 += ww2.format(space = ' ')
+            buffSize += wSize + 1
+
+
+    # Print the last line
+    # Properly end text formating
+    if isUsingColor > 0:
+        buff1 += colors.END
+        buff2 += colors.END
+
+    # Display buffers
+    print(" {}{} | {}".format(buff1, ' '*(colWidth-buffSize), buff2))
